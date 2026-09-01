2f5a82ddeb8d550c202a7105c101b7d728e8ebf0
tausif7776 <tausif.raza7776@gmail.com>
2026-09-01 19:20:11 +0530

    first-commit

--- DAY-01/index.js
+++ DAY-01/index.js
@@ -2,14 +2,69 @@
 
 
 // JSX : javascript XML: look like html
-const element = <h1 id="title">Hello JavaScript</h1>
+// const element = <h1 id="title">Hello JavaScript</h1>
 
-const element2 = <div>
-    <li>Good</li>
-    <li>Morning</li>
-</div>
+// Good Practise
+// const element2 = (<div>
+//     <li>Good</li>
+//     <li>Morning</li>
+// </div>);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const element3 = <>
+//     <h1>Hello Buddy</h1>
+//     <h2>JavaScript & React.js</h2>
+// </>
+
+// function App(name) {
+//     return (
+//         <h1>Helllo JavaScript {name}</h1>
+//     );
+// }
+
+// const a = App("Coder");
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(element);
-root.render(element2);
+// root.render(element2);
+// root.render(element3);
+// root.render(App());
+// root.render(a)
+
+//const age = 22;
+//const isLogedIn = true;
+//const element2 = <h3>Buddy You are {isLogedIn?<p>Logged In</p>:<p>Kindly SignIn</p>}</h3>
+//const element = <h1>Hello Coder {age>25?"Adult":"Kid"}</h1>
+
+// const course = ["HTML", "CSS", "JavaScript", "React"];
+
+// const element = (
+//     <ul>
+//         {course.map(courses=><li>{courses}</li>)}
+//     </ul>
+// );
+
+
+// function App(props) {
+
+//     return (
+//         <h1>Hello Bhai {props.name} {props.age}</h1>
+//     )
+// }
+
+
+// //     name:"Tausif",
+// //     age:22
+// // }
+
+// const element = <App name="Tausif" age={22}></App>
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+// // root.render(element2);
+
+const element = <h1>Hello Weolcome to js</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
---
+++ DAY-02/app.js
@@ -0,0 +1,3 @@
+const element = <h1>Welcome to React JS Course!</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
