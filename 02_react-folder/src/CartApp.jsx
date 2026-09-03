02c13276c62804cf74fc9a1fc9c15d9fa45625db
tausif7776 <tausif.raza7776@gmail.com>
2026-09-03 21:34:26 +0530

    commit-intial

---
+++ 02_react-folder/src/CartApp.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Buddy!</h1>
+      <p>What's up?</p>
+      <p>
+        i'm prrrty good and about😍.
+      </p>
+    </>
+  )
+}
+
+
+export default App;
