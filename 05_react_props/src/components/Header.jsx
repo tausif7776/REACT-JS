1b837d8abd978447564d2f7b2bb6d009ac13c105
tausif7776 <tausif.raza7776@gmail.com>
2026-09-10 22:20:36 +0530

    intial-commit

---
+++ 05_react_props/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My App</h1>
+      <h2>React Props Example</h2>
+      <h3>Learn how to use props in React components</h3>
+      <h4>Props allow you to pass data from parent to child components</h4>
+      <h5>Props are read-only and cannot be modified by the child component</h5>
+      <h6>Props make components reusable and flexible</h6>
+    </header>
+  );
+}
+
+export default Header;
--- 05_react_props/src/main.jsx
+++ 05_react_props/src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Card from './components/Card'
+import Header from './components/Header'
 
 createRoot(document.getElementById('root')).render(
-  <Card/>
+  <StrictMode>
+    <Card/>
+    <Header/>
+  </StrictMode>
 )
