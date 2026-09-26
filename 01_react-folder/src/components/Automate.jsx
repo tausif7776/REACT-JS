737f377b5b1f1de69c9b792901dd873817acbe5d
tausif7776 <tausif.raza7776@gmail.com>
2026-09-26 22:21:00 +0530

    first

---
+++ 01_react-folder/src/components/Automate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Automate = () => {
+  return (
+    <div>
+        <div>Automate</div>
+        <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default Automate
